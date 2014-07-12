12f3fad845bb0b3469cded33a9daf7a955035e24
Carlos Lazo <carlos.jose.lazo@gmail.com>
2014-07-12 00:06:08 -0400

    Adds in /ping route for basic health check for NewRelic.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,4 +2,8 @@ class StaticController < ApplicationController
   def index
     render :text => 'Hello World! ==> From RTJ-BackEnd.'
   end
+
+  def ping
+    render :text => 'pong'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static#index'
-
+  
+  get '/ping',  to: 'static#ping' # Health Check
   get '/plans', to: 'plan#index'
 end
