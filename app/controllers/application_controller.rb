18ba78515b260417d17822fbe9a12628ccd55743
Carlos Lazo <carlos.jose.lazo@gmail.com>
2014-07-12 23:12:59 -0400

    Sets CORS origin based on current Rails environment.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,9 @@ class ApplicationController < ActionController::Base
 
   # If this is a preflight OPTIONS request, then short-circuit the request,
   # return only the necessary headers and return an empty text/plain.
-
   def cors_preflight_check
     if request.method == :options
-      headers['Access-Control-Allow-Origin']  = ENV['CROSS_DOMAIN_LIST']
+      headers['Access-Control-Allow-Origin']  = cors_origin
       headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
       headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
       headers['Access-Control-Max-Age']       = '1728000'
@@ -19,8 +18,18 @@ class ApplicationController < ActionController::Base
 
   # For all responses in this controller, return the CORS access control headers.
   def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin']  = ENV['CROSS_DOMAIN_LIST']
+    headers['Access-Control-Allow-Origin']  = cors_origin
     headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
     headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  # Based on Rails.env, allow specific front-end to post.
+  def cors_origin
+    case Rails.env
+    when 'development'
+      ENV['CROSS_DOMAIN_DEV']
+    when 'production'
+      ENV['CROSS_DOMAIN_PROD']
+    end
   end
 end
