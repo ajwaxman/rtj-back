d203d2645142c6c8fe19252aeb237f93a2c92bc1
Carlos Lazo <carlos.jose.lazo@gmail.com>
2014-07-12 20:35:54 -0400

    Adds Cross-Origin Resource Sharing (CORS) capability for front-end access.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+  before_filter :cors_preflight_check
+  after_filter  :cors_set_access_control_headers
+
+  # If this is a preflight OPTIONS request, then short-circuit the request,
+  # return only the necessary headers and return an empty text/plain.
+
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin']  = 'http://localhost:1212'#ENV['CROSS_DOMAIN_LIST']
+      headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+      headers['Access-Control-Max-Age']       = '1728000'
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
+
+  # For all responses in this controller, return the CORS access control headers.
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin']  = 'http://localhost:1212'#ENV['CROSS_DOMAIN_LIST']
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  # Default Message at Home Page
   root 'static#index'
-  
+  # Heartbeat Route for NewRelic
   get '/ping',  to: 'static#ping' # Health Check
+
   get '/plans', to: 'plan#index'
 end
